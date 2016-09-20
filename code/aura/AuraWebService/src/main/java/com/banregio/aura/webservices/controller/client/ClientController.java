cb6173fdd6280f099256de7b432c4adb9769d9f3
FernandoLuna <fernando@luna.local>
2016-09-19 20:27:10 -0500

    add url api v1

--- code/aura/AuraWebService/src/main/java/com/banregio/aura/webservices/controller/client/ClientController.java
+++ code/aura/AuraWebService/src/main/java/com/banregio/aura/webservices/controller/client/ClientController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller("clientController")
 @Scope("prototype")
-@RequestMapping("/client")
+@RequestMapping("/api/v1/client")
 public class ClientController {
 
   @Autowired
